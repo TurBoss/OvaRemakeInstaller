977f9b926f9f4c86cdb9136e3f39b82b651977fb
TurBoss <j.l.toledano.l@gmail.com>
2017-01-22 23:39:33 +0100

    start working, needs to step operations

--- config/controlscript.js
+++ config/controlscript.js
@@ -1,10 +1,11 @@
-var Dir = new function () {
+var Dir = new function ()
+{
     this.toNativeSparator = function (path) {
         if (systemInfo.productType === "windows")
             return path.replace(/\//g, '\\');
         return path;
     }
-};
+}
 
 function Controller()
 {
--- packages/com.ovaremake.backend/meta/installscript.js
+++ packages/com.ovaremake.backend/meta/installscript.js
@@ -1,24 +1,24 @@
-var Dir = new function () {
+var Dir = new function ()
+{
     this.toNativeSparator = function (path) {
         if (systemInfo.productType === "windows")
             return path.replace(/\//g, '\\');
         return path;
     }
-};
+}
 
-function Component() {
-	
-
+function Component()
+{
     component.loaded.connect(this, Component.prototype.installerLoaded);
 }
 
 Component.prototype.installerLoaded = function()
 {
-
 	installDisk = "D:\\";
 	installPath = "C:\\Games\\FF7 OVA Remake";
-	var installationCanceled = false;
-
+	installationCanceled = false;
+	
+	/*
 	var ff7 = 0;
 
 	if (systemInfo.productType === "windows"){
@@ -45,7 +45,8 @@ Component.prototype.installerLoaded = function()
 			installationCanceled = true;
 		}
 	}
-
+	*/
+	
 	if (!installationCanceled){
 		if (installer.addWizardPage(component, "TargetWidget", QInstaller.TargetDirectory)) {
 			var widget = gui.pageWidgetByObjectName("DynamicTargetWidget");
@@ -68,24 +69,26 @@ Component.prototype.installerLoaded = function()
 	}
 }
 
-Component.prototype.installTarget = function () {
+Component.prototype.installTarget = function ()
+{
+		
+	var diskPath = QFileDialog.getExistingDirectory("Choose your FF7 Install Disk.", "D:\\");
+		
     var widget = gui.pageWidgetByObjectName("DynamicTargetWidget");
-    if (widget != null) {
-		
-		var diskPath = QFileDialog.getExistingDirectory("Choose your FF7 Install Disk.", "C:");
-		
-        if (diskPath != "") {
+    if (widget !== null) {
+        if (diskPath !== "") {
             widget.installDirectory.text = Dir.toNativeSparator(diskPath);
-			installDisk = Dir.toNativeSparator(diskPath);
+			//installDisk = Dir.toNativeSparator(diskPath);
         }
     }
 }
 
-Component.prototype.installChanged = function (path) {
+Component.prototype.installChanged = function (path)
+{
     var widget = gui.pageWidgetByObjectName("DynamicTargetWidget");
-    if (widget != null) {
-        if (path != "") {
-            if (installer.fileExists(path + "/FF7inst.exe")) {
+    if (widget !== null) {
+        if (path !== "") {
+            if (installer.fileExists(path + "//FF7inst.exe")) {
 				installDisk = Dir.toNativeSparator(path);
 				
 				var msg = "<font color='green'>" + qsTr("installation disc found.") + "</font>";
@@ -103,13 +106,14 @@ Component.prototype.installChanged = function (path) {
     }
 }
 
-Component.prototype.chooseTarget = function () {
+Component.prototype.chooseTarget = function ()
+{
     var widget = gui.pageWidgetByObjectName("DynamicTargetWidget");
-    if (widget != null) {
+    if (widget !== null) {
         
         var newTarget = QFileDialog.getExistingDirectory("Choose your target directory.", Component.prototype.installPath);
         
-        if (newTarget != "") {
+        if (newTarget !== "") {
             widget.targetDirectory.text = Dir.toNativeSparator(newTarget);
             installPath = Dir.toNativeSparator(newTarget);
         }
@@ -117,10 +121,11 @@ Component.prototype.chooseTarget = function () {
     }
 }
 
-Component.prototype.targetChanged = function (path) {
+Component.prototype.targetChanged = function (path)
+{
     var widget = gui.pageWidgetByObjectName("DynamicTargetWidget");
-    if (widget != null) {
-        if (path != "") {
+    if (widget !== null) {
+        if (path !== "") {
             installer.setValue("TargetDir", path);
             installPath = Dir.toNativeSparator(path);
         }
@@ -129,81 +134,82 @@ Component.prototype.targetChanged = function (path) {
 		}
     }
 }
-/*
-Component.prototype.createOperations = function() {
+
+Component.prototype.createOperations = function()
+{
     component.createOperations();
+    
+	QMessageBox["warning"]( "warning", "CD", "Please inser game disk");
 
     if (installer.isInstaller()) {
         if (systemInfo.productType === "windows") {
 			
-			installer.gainAdminRights();
-			
-			copyInstallationFiles(installDisk, installPath, 0));
-			copyInstallationFiles(installDisk, installPath, 1));
-			copyInstallationFiles(installDisk, installPath, 2));
-			copyInstallationFiles(installDisk, installPath, 3));
+			copyInstallationFiles(installDisk, installPath, 0);
+			copyInstallationFiles(installDisk, installPath, 1);
+			copyInstallationFiles(installDisk, installPath, 2);
+			copyInstallationFiles(installDisk, installPath, 3);
 			
 			component.registerPathForUninstallation(installPath + "\\data", true);
 			component.registerPathForUninstallation(installPath + "\\movies", true);
 			
-			createSetupRegistryKeys();
+			//createSetupRegistryKeys();
 			
-			//component.addElevatedOperation("Execute", "{0}", "workingdirectory=@TargetDir@", "@TargetDir@/runff7config.bat");
+			//component.performOperation("Execute", "{0}", "workingdirectory=@TargetDir@", "@TargetDir@/runff7config.bat");
 			
-			createOpenGLRegistryKeys();
+			//createOpenGLRegistryKeys();
         }
     }
-    else if (installer.isUninstaller()) {
-		QMessageBox["warning"]( "warning", "CD", "Please inser game disk" );
-		//component.addElevatedOperation("Execute", "cmd", "/C", "echo", "do nothing", "UNDOEXECUTE", "cmd", "/C", "reg", "delete", "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Software\test","/f")
-	}
 }
 
-copyInstallationFiles = function(diskPath, gamePath, diskNo) {
+var copyInstallationFiles = function(diskPath, gamePath, diskNo)
+{
 	if (diskNo == 0) {
 		diskPath = diskPath + "\\data";
 		gamePath = gamePath + "\\data";
-		component.addElevatedOperation("Mkdir", gamePath);
-		component.addElevatedOperation("CopyDirectory", diskPath, gamePath);
+		component.performOperation("Mkdir", gamePath);
+		component.performOperation("CopyDirectory", diskPath, gamePath);
 	}
 	else if (diskNo != 0){
 		QMessageBox["warning"]( "warning", "CD", "Please inser game disk " + diskNo );
 		diskPath = diskPath + "\\movies";
 		gamePath = gamePath + "\\movies";
-		component.addElevatedOperation("Mkdir", gamePath);
-		component.addElevatedOperation("CopyDirectory", diskPath, gamePath);
+		component.performOperation("Mkdir", gamePath);
+		component.performOperation("CopyDirectory", diskPath, gamePath);
 	}
-	
 }
-createSetupRegistryKeys = function() {
+
+var createSetupRegistryKeys = function()
+{
 	
-	component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "AppPath", Dir.toNativeSparator(installer.value("TargetDir")) + "\\");
-	component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DataPath", Dir.toNativeSparator(installer.value("TargetDir")) + "\\data\\");
-	component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DataDrive", component.installDisk + "\\");
-	component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "MoviePath", component.installDisk + "\\FF7\\MOVIES\\");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "AppPath", Dir.toNativeSparator(installer.value("TargetDir")) + "\\");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DataPath", Dir.toNativeSparator(installer.value("TargetDir")) + "\\data\\");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DataDrive", component.installDisk + "\\");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "MoviePath", component.installDisk + "\\FF7\\MOVIES\\");
 	
-	component.addElevatedOperation("Execute", "workingdirectory=@TargetDir@", "{0}", "cmd", "/C", "reg", "import", "@TargetDir@\\Tools\\ff7_install_OVA.reg");
+	component.performOperation("Execute", "workingdirectory=@TargetDir@", "{0}", "cmd", "/C", "reg", "import", "@TargetDir@\\Tools\\ff7_install_OVA.reg");
 
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DiskNo", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "FullInstall", 0);
+	/*
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "DiskNo", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "FullInstall", 0);
 
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/DD_GUID", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Driver", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/DriverPath", "ff7_opengl.fgd");
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Mode", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Options", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/DD_GUID", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Driver", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/DriverPath", "ff7_opengl.fgd");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Mode", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Graphics/Options", 0);
 
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MIDI_data", "GENERAL_MIDI");
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MIDI_DeviceID", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MusicVolume", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/Options", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MIDI_data", "GENERAL_MIDI");
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MIDI_DeviceID", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/MusicVolume", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/MIDI/Options", 0);
 
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/Options", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/SFXVolume", 0);
-	//component.addElevatedOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/Sound_GUID", 0);
-
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/Options", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/SFXVolume", 0);
+	component.performOperation("GlobalConfig", "SystemScope", "JauriaStudios INC", "FF VII OVA Remake", "1.00/Sound/Sound_GUID", 0);
+	*/
 }
-* 
-createOpenGLRegistryKeys = function() {
-	component.addElevatedOperation("Execute", "workingdirectory=@TargetDir@", "{0}", "cmd", "/C", "reg", "import", "@TargetDir@\\Tools\\ff7_opengl_OVA.reg");
-}*/
+
+var createOpenGLRegistryKeys = function()
+{
+	component.performOperation("Execute", "workingdirectory=@TargetDir@", "{0}", "cmd", "/C", "reg", "import", "@TargetDir@\\Tools\\ff7_opengl_OVA.reg");
+}
